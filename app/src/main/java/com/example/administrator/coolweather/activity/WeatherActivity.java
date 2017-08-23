fda0ee32315e94267210801ba50341d3beaacc68
Nim <649783690@qq.com>
2017-08-23 15:12:44 +0800

    Added the ability to automatically update the weather in the background.

--- app/src/main/java/com/example/administrator/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/administrator/coolweather/activity/WeatherActivity.java
@@ -12,6 +12,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.administrator.coolweather.R;
+import com.example.administrator.coolweather.service.AutoUpdateService;
 import com.example.administrator.coolweather.util.HttpUtil;
 import com.example.administrator.coolweather.util.Utility;
 
@@ -168,5 +169,7 @@ public class WeatherActivity extends Activity implements View.OnClickListener {
         currentDateText.setText(prefs.getString("current_date", ""));
         weatherInfoLayout.setVisibility(View.VISIBLE);
         cityNameText.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
