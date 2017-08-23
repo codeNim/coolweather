3d9fe83b46b9ee4ee984793b2ea4b12ab27c4206
Nim <649783690@qq.com>
2017-08-23 13:30:26 +0800

    add DatabaseHelper class and others.

---
+++ app/src/main/java/com/example/administrator/coolweather/model/Province.java
@@ -0,0 +1,29 @@
+package com.example.administrator.coolweather.model;
+
+/**
+ * Created by Administrator on 2017/8/23.
+ */
+
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getProvinceName() {
+        return provinceName;
+    }
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
